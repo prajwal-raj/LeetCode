2e4cc72b2feb6cad6a7a24f5dbf6945bcd823e5f
Prajwal Raj <prajwalraj224@gmail.com>
2023-11-24 18:12:26 +0530

    Create LC_2_sum.py

---
+++ LC_2_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        dict_nums = {}
+        
+        for idx, num in enumerate(nums):
+            dict_nums[num] = idx
+        
+        for idx, num in enumerate(nums):
+            if target - num in dict_nums and idx != dict_nums[target - num]:
+                return [idx, dict_nums[target - num]]
